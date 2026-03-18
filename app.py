56ba1de6d66bad426128ad28303289c1466c3b1e
shengsixu359-cloud <shengsixu359@gmail.com>
2026-03-18 19:25:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="店舗KPIダッシュボード", layout="wide")
 st.title("📊 店舗KPIダッシュボード")
 
 # スプレッドシートのURL
-SPREADSHEET_URL = "[https://docs.google.com/spreadsheets/d/1iwGSIWU8aEoW82hzZCI6YO8VufhAc3zR8KnS4OPPMQA/edit#gid=244052959](https://docs.google.com/spreadsheets/d/1iwGSIWU8aEoW82hzZCI6YO8VufhAc3zR8KnS4OPPMQA/edit#gid=244052959)"
+SPREADSHEET_URL = "(https://docs.google.com/spreadsheets/d/1iwGSIWU8aEoW82hzZCI6YO8VufhAc3zR8KnS4OPPMQA/edit?gid=0#gid=0)"
 
 @st.cache_data(ttl=300)
 def load_all_sheets():
